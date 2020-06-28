eda31b6fadb49400b64a584ee43ba5d9ea89b614
Rocky Lai <rockylai@shuiguzi.com>
2020-06-28 15:50:06 +0800

    add framework

---
+++ Assets/Scripts/RootControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class RootControl : MonoBehaviour
+{
+    private ARRaycastManager ARRaycast;
+    private ARAnchorManager anchorManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    private ARAnchor anchor;
+
+    void Start()
+    {
+        ARRaycast = GetComponent<ARRaycastManager>();
+        anchorManager = GetComponent<ARAnchorManager>();
+    }
+
+    void Update()
+    {
+        if (anchor != null)
+        {
+            return;
+        }
+
+        var touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Ended)
+        {
+            return;
+        }
+
+        if (ARRaycast.Raycast(touch.position, s_Hits, TrackableType.FeaturePoint))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
+            anchor = anchorManager.AddAnchor(hitPose);
+        }
+    }
+}
